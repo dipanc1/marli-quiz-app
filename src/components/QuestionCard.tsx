b1329ae2d0ad736a64b1aaad7a1b4f1047899ba0
dipanc1 <dipanchhabra@gmail.com>
2022-01-13 17:26:14 +0530

    react-quiz-app--typescript

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,32 @@
+
+
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: string;
+    questionNr: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+    question,
+    answer,
+    callback,
+    userAnswer,
+    questionNr,
+    totalQuestions
+}) => {
+    return (
+        <div>
+            <p className="number">
+                Question:  {questionNr} /{totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{ __html: question }}>
+
+            </p>
+        </div>
+    )
+}
+
+export default QuestionCard;
